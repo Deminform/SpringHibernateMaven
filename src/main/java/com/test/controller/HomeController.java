2b15567d6b5329e0420ca407e9591acada48b7ce
Demchenko Sergey <deminform@gmail.com>
2016-04-17 19:32:35 +0300

    Adding dependency's 17.04.2016 19:32

--- src/main/java/com/test/controller/HomeController.java
+++ src/main/java/com/test/controller/HomeController.java
@@ -3,9 +3,6 @@ package com.test.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-/**
- * Created by EBPO on 17.04.2016.
- */
 @Controller
 public class HomeController {
     @RequestMapping(value = "/")
